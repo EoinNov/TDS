23c21bb115b449a4178494a0281d1130c792c0ef
Suinavi <novivvan@gmail.com>
2021-03-18 19:25:01 +0300

    Initial commit

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    public float followDistance;
+    public float attackDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+
+        Gizmos.DrawWireSphere(transform.position, followDistance);
+
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, attackDistance);
+
+    }
+}
---
+++ Assets/Scripts/ZombieMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMove : MonoBehaviour
+{
+    public float speed;
+    public Player player;
+    Rigidbody2D rb;
+    Animator anim;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 zombiePoition = transform.position;
+        Vector3 playerPosition = player.transform.position;
+
+        Vector3 direction = playerPosition - zombiePoition;
+        Move(direction);
+        Rotate(direction);
+
+    }
+
+  
+
+    void Move(Vector3 direction)
+    {
+
+        anim.SetFloat("speed", rb.velocity.magnitude);
+        rb.velocity = direction * speed;
+
+
+
+    }
+
+    void Rotate(Vector3 direction)
+    {
+        
+        direction.z = 0;
+        transform.up = -direction;
+    }
+    private void OnDisable()
+    {
+        rb.velocity = Vector2.zero;
+    }
+}
